4c48a3752019af20b687cdc4d27c2ce50ffcfc49
infocollegechampagneur <informatique@champagneur.qc.ca>
2025-10-15 12:14:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-rom fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Optional
